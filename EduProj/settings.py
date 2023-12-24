be23a56215a685850f120a2ddcdd74f8ddd21e5d
Jmint <jeffreymintah737@gmail.com>
2023-12-24 22:28:19 +0000

    reddon

--- EduProj/settings.py
+++ EduProj/settings.py
@@ -30,8 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = False
+if ENVIRONMENT == 'development':
+    DEBUG = True
+else:
+    DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
