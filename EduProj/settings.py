d7a9a3a7f052257c487554b2453bbec69a45564c
Jmint <jeffreymintah737@gmail.com>
2023-12-28 11:55:15 +0000

    added the '9' to the api key value

--- EduProj/settings.py
+++ EduProj/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if ENVIRONMENT == 'development': #switch to production for changes everytime to see the errors
+if ENVIRONMENT == 'production': #switch to development for changes everytime to see the errors
     DEBUG = True
 else:
     DEBUG = False
